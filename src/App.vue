5a26d1fba65f79f6c1567b2655110a7e354ba669
Edson Alexandre <edson.a.s.silveira@gmail.com>
2023-06-14 13:48:59 -0300

    Evoluindo chat

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <button @click="sendMessage()">Emitir Mensagem</button>
+  <div class="App">
+    <form @submit.prevent="submitToken">
+      <input type="text" placeholder="Enter Name" v-model="token.name" />
+      <button type="submit">Submit</button>
+    </form>
+    <div class="box">
+      <div class="messages">
+        <div v-for="user in messages" :key="user.id">{{ user.name }}: {{ user.message }}</div>
+      </div>
+      <div class="messages"></div>
+      <form class="input-div" @submit.prevent="submitMessage">
+        <input type="text" placeholder="Type in text" v-model="inputMessageText" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -11,14 +23,42 @@ import SocketioService from './services/socketio.service.js';
 export default {
   name: 'App',
   components: {},
+  data() {
+    return {
+      token: {
+        id: '',
+        name: '',
+        iat: 123,
+      },
+      inputMessageText: '',
+      messages: [],
+    };
+  },
   methods: {
-    sendMessage() {
-      SocketioService.sendMessage();
+    submitToken() {
+      console.log(this.token);
+      SocketioService.setupSocketConnection(this.token);
+      SocketioService.subscribeToMessages((err, data) => {
+        console.log(data);
+        this.messages.push(data);
+      });
+    },
+    submitMessage() {
+      const CHAT_ROOM = 'myRandomChatRoomId';
+      const message = this.inputMessageText;
+      SocketioService.sendMessage({ message, roomName: CHAT_ROOM }, cb => {
+        console.log(cb);
+        this.messages.push({
+          message,
+          ...this.token,
+        });
+        this.inputMessageText = '';
+      });
     },
   },
-  created() {
-    SocketioService.setupSocketConnection();
-  },
+  // created() {
+  //   SocketioService.setupSocketConnection();
+  // },
 
   beforeUnmount() {
     SocketioService.disconnect();
@@ -27,12 +67,25 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.App {
+  padding: 1rem;
+}
+
+.box {
+  width: fit-content;
+  height: 400px;
+  border: solid 1px #000;
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+}
+
+.messages {
+  flex-grow: 1;
+}
+
+.input-div {
+  display: flex;
+  width: 100%;
 }
 </style>
--- src/services/socketio.service.js
+++ src/services/socketio.service.js
@@ -4,17 +4,13 @@ class SocketioService {
   socket;
   constructor() {}
 
-  setupSocketConnection() {
-    const uuid = self.crypto.randomUUID();
-
+  setupSocketConnection(token) {
     this.socket = io(process.env.VUE_APP_SOCKET_ENDPOINT, {
       auth: {
-        token: uuid,
+        token,
       },
     });
-    this.socket.on('my broadcast', data => {
-      console.log(data);
-    });
+    console.log(`Connecting socket...`);
   }
 
   disconnect() {
@@ -22,8 +18,16 @@ class SocketioService {
       this.socket.disconnect();
     }
   }
-  sendMessage() {
-    this.socket.emit('my message', 'Hello there from Vue.');
+  subscribeToMessages(cb) {
+    if (!this.socket) return true;
+    this.socket.on('message', msg => {
+      console.log('Room event received!');
+      return cb(null, msg);
+    });
+  }
+
+  sendMessage({ message, roomName }, cb) {
+    if (this.socket) this.socket.emit('message', { message, roomName }, cb);
   }
 }
 
